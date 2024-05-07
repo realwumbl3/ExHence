58f28453e177843af13547fae274e620f4692412
realwumbl3 <wumbl3@gmail.com>
2024-05-07 17:08:47 -0400

    proogress

--- background.js
+++ background.js
@@ -1,20 +1,5 @@
-async function getCurrentTab() {
-	let queryOptions = { active: true, lastFocusedWindow: true };
-	// `tab` will either be a `tabs.Tab` instance or `undefined`.
-	let [tab] = await chrome.tabs.query(queryOptions);
-	return tab;
-}
-
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-
-	console.log("onMessage", request, sender, sendResponse);
-
-	if (request.ping === "ping") {
-		sendResponse({ pong: "pong" });
-	}
-
+	console.log("onMessage", { request, sender, sendResponse });
+	if (request === "getTab") sendResponse({ id: sender.tab.id })
+	return true;
 });
-
-(async () => {
-	console.log("getCurrentTab", await getCurrentTab());
-})();
--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-const eXHentaiCtrl = new (class exHentaiCtrl {
+new (class exHentaiCtrl {
 	constructor() {
+		this.active = false
 		this.gallery = {
 			container: null,
 			columns: null,
-			ready: false,
 			prev: null,
 			next: null,
 		};
@@ -18,83 +18,109 @@ const eXHentaiCtrl = new (class exHentaiCtrl {
 			index: 0,
 			thisPage: null,
 			lastPage: null,
-			lastGallery: [],
+			galleryHistory: [],
 		};
-		this.attachHeader();
-
+		this.thisTabID = null;
 		window.addEventListener("keydown", this.keydown.bind(this));
+		observe(document, "#nb", this.attachHeader.bind(this))
+		observe(document, "#gdt, .itg.gld", this.initGallery.bind(this))
+		observe(document, ".sni", this.initView.bind(this))
+	}
 
-		this.loadState();
-		chrome.runtime.sendMessage({ ping: "ping" }, (response) => {
-			console.log(response.pong);
+	async getCurrentTabContext() {
+		return new Promise((res, rej) => {
+			chrome.runtime.sendMessage("getTab", (response) => {
+				if (!response.hasOwnProperty("id")) return console.log("no tab response");
+				res(response.id)
+			});
 		});
 	}
 
-	saveState() {
-		this.state.lastGallery = this.state.lastGallery.splice(-20);
-		chrome.storage.local.set({ state: this.state }, () => {
-			console.log("saved state ", this.state);
-		});
+	async loadTab(thisTabID) {
+		return new Promise((res, rej) => {
+			this.thisTabID = thisTabID;
+			const stateId = `${this.thisTabID}-state`
+			chrome.storage.local.get([stateId], (result) => {
+				if (!result[stateId]) return console.log("no init state set yet");
+				this.state = result[stateId]
+				console.log("[ExHentaiCTRL] | restored state ", this.state);
+				res(true)
+			});
+		})
 	};
 
-	async loadState() {
-		chrome.storage.local.get(["state"], (result) => {
-			if (!result.state) return console.log("no init state set yet");
-			console.log("restored state ", result.state);
-			this.state = result.state;
-		});
-	};
-
-	initPage() {
+	async initGallery(gallery) {
+		const tabId = await this.getCurrentTabContext();
+		await this.loadTab(tabId);
+		console.log("Am i initiating?", this)
 		const path = window.location.href.split(window.location.origin)[1];
-		let target = null;
-		if ((target = document.querySelector("#gdt, .itg.gld"))) {
-			this.initGalleryView(target);
-			this.state.lastGallery = this.state.lastGallery.filter((_) => _ !== this.state.thisPage);
-			this.state.lastGallery.push(path);
+		this.state.thisPage = path;
+		if (gallery.matches("#gdt,.itg.gld")) {
+			this.initGalleryView(gallery);
+			console.log({ path })
+			if (this.state.galleryHistory.length === 0 || this.state.galleryHistory[0] !== path) this.state.galleryHistory.unshift(path)
+			// this.state.lastGallery = this.state.lastGallery.filter((_) => _ !== this.state.thisPage);
 			this.saveState();
-		} else if ((target = document.querySelector(".sni"))) {
-			this.enableVIEW(target);
+			return
 		}
+
 	};
 
-	attachHeader() {
-		observe(
-			document,
-			"#nb",
-			(_) => {
-				oM()(
-					html`
-						<div><a class="nbw" href="">exHentai-CTRL Options</a></div>
-					`
-				).appendTo(_);
-			},
-			true
-		);
+	async initView(view) {
+		const tabId = await this.getCurrentTabContext();
+		await this.loadTab(tabId);
+		console.log("view????", view)
+		this.enableVIEW(view);
+	}
+
+	saveState() {
+		this.state.galleryHistory = this.state.galleryHistory.splice(0, 20);
+		chrome.storage.local.set({ [`${this.thisTabID}-state`]: this.state }, () => {
+			console.log("[ExHentaiCTRL] | saved state ", this.state);
+		});
+	};
+
+	attachHeader(exheader) {
+		zyX.html`
+				<div><a class="nbw" href="">exHentai-CTRL Options</a></div>
+				<div><span this=log class="nbw">Log Extension.</span></div>
+		`
+			.appendTo(exheader)
+			.pass(({ log }) => {
+				log.addEventListener("click", (e) => {
+					console.log("[ExHentaiCTRL] | this ", this);
+				})
+			})
 	};
 
 	enableVIEW() {
-		this.gallery.ready = true;
+		this.active = true;
 	};
 
 	initGalleryView(target) {
-		console.log("initGalleryView", target);
+		console.log("[ExHentaiCTRL] | initGalleryView", target);
 		this.gallery.container = target;
 		this.readNavBar();
 		this.getGalleryNodes();
 		const firstThumbnail = firstInView(this.gallery.nodes);
-		// remember selected thumbnail for each gallery
+		// TODO: remember selected thumbnail for each gallery
 		this.selectThumbnail(firstThumbnail);
-		this.gallery.ready = true;
+		this.active = true;
 	};
 
 	readNavBar() {
-		const navBar = [...document.querySelectorAll("#uprev,#unext")]
-		console.log("navBar", navBar);
-		this.gallery.prev = navBar[0]?.href;
-		this.gallery.next = navBar[1]?.href;
-		// .ptds <- active page
-		// .ptdd <- unavailable
+		const galleryNavBar = [...document.querySelectorAll("#uprev,#unext")]
+		if (galleryNavBar.length === 2) {
+			console.log("[ExHentaiCTRL] | read gallery navBar");
+			this.gallery.prev = galleryNavBar[0]?.href;
+			this.gallery.next = galleryNavBar[1]?.href;
+		}
+		else {
+			console.log("[ExHentaiCTRL] | read post pages navBar")
+			const comicNavBar = [...document.querySelectorAll("table.ptt>tbody>tr>td")]
+			this.gallery.prev = comicNavBar[0].firstChild?.href;
+			this.gallery.next = comicNavBar[comicNavBar.length - 1].firstChild?.href;
+		}
 	}
 
 	getGalleryNodes() {
@@ -114,17 +140,12 @@ const eXHentaiCtrl = new (class exHentaiCtrl {
 
 	keydown(e) {
 		if (document.body.querySelector("input:focus")) return; // ignore if any input feild is focused
-		if (!this.gallery.ready) return false;
 		switch (e.code) {
 			case "KeyE":
 				this.pressEonThumb();
 				break;
 			case "KeyQ":
 				this.pressQ();
-				break;
-			case "KeyB":
-				console.log("eXHentaiCtrl");
-				console.log(this);
 				break;
 			case "KeyW":
 			case "KeyA":
@@ -142,6 +163,7 @@ const eXHentaiCtrl = new (class exHentaiCtrl {
 	};
 
 	moveHighlight(e) {
+		if (!this.active) return false;
 		this.calculateGrid();
 		let nodeIndex = this.gallery.nodes.indexOf(this.thumbnail.active);
 		// console.log("columns", this.gallery.columns, "nodeIndex", nodeIndex);
@@ -159,7 +181,6 @@ const eXHentaiCtrl = new (class exHentaiCtrl {
 			case "ArrowDown":
 				if (nodeIndex > this.gallery.nodes.length - this.gallery.columns - 1) {
 					// ALREADY ON LAST ROW
-					next = this.gallery.nodes[this.gallery.nodes.length - 1];
 					break;
 				}
 				nodeIndex += this.gallery.columns;
@@ -167,29 +188,23 @@ const eXHentaiCtrl = new (class exHentaiCtrl {
 			case "KeyA": // LEFT
 			case "ArrowLeft":
 				if (nodeIndex === 0 || nodeIndex % this.gallery.columns === 0) {
-					// ALREADY ON FIRST COLUMN
-
+					// ALREADY ON FIRST COLUMN OR FIRST THUMBNAIL
 					if (this.pressAonFirst()) return;
-				}
-				if (nodeIndex <= 0) {
-					// ALREADY ON FIRST THUMBNAIL
-					break;
 				}
 				nodeIndex--;
 				break;
 			case "KeyD": // RIGHT
 			case "ArrowRight":
-				if (nodeIndex > 0 && (nodeIndex + 1) % this.gallery.columns === 0) {
-					// ALREADY ON LAST COLUMN
+				if ((nodeIndex + 1) % this.gallery.columns === 0
+					|| nodeIndex === this.gallery.nodes.length - 1
+				) {
+					// ALREADY ON LAST COLUMN OR LAST THUMBNAIL
 					if (this.pressDonLast()) return;
-				}
-				if (nodeIndex >= this.gallery.nodes.length - 1) {
-					// ALREADY ON LAST THUMBNAIL
-					break;
 				}
 				nodeIndex++;
 				break;
 		}
+		nodeIndex = Math.max(0, Math.min(this.gallery.nodes.length - 1, nodeIndex));
 		this.selectThumbnail(this.gallery.nodes[nodeIndex]);
 		// console.log("post, nodeIndex", this.gallery.nodes.indexOf(this.thumbnail.active));
 	};
@@ -197,30 +212,28 @@ const eXHentaiCtrl = new (class exHentaiCtrl {
 	pressEonThumb() {
 		const thumbnailAnchor = this.thumbnail.active.querySelector("a");
 		const thumbnailLink = thumbnailAnchor.href;
-		this.saveState();
+		// this.saveState();
 		window.location = thumbnailLink;
 	};
 
 	pressQ() {
-		// if on post use V button at bottom of page.
-		// if on gallery, first and last thumbnail navigate pages and Q returns to last gallery
-
-		if (this.state.lastGallery.length > 0) {
-			for (const historyUrl of this.state.lastGallery.reverse()) {
-				if (historyUrl !== this.state.thisPage) {
-					this.state.lastGallery.splice(this.state.lastGallery.indexOf(historyUrl), 1);
-					this.saveState();
-					window.location = historyUrl;
-					return;
-				}
-			}
+		if (!this.active) return
+		if (this.state.galleryHistory.length > 0) {
+			const previous = this.state.galleryHistory.find(_ => _ !== this.state.thisPage)
+			if (!previous) return console.warn("no history to return to")
+			this.state.galleryHistory = this.state.galleryHistory.splice(this.state.galleryHistory.indexOf(previous) + 1)
+			this.saveState();
+			console.log("going to", previous)
+			this.active = false // set to false so you don't interrupt the page change with another page change		
+			window.location = previous;
+			return;
 		}
 	};
 
 	pressAonFirst() {
-		console.log("pressAonFirst");
+		console.log("key(Left) on first thumb")
 		if (this.options.firstLastColumnPageNav && this.gallery.prev) {
-			this.gallery.ready = false;
+			this.active = false // set to false so you don't interrupt the page change with another page change		
 			window.location = this.gallery.prev;
 			return true;
 		}
@@ -228,9 +241,9 @@ const eXHentaiCtrl = new (class exHentaiCtrl {
 	};
 
 	pressDonLast() {
-		console.log("pressDonLast");
+		console.log("key(Right) on last thumb")
 		if (this.options.firstLastColumnPageNav && this.gallery.next) {
-			this.gallery.ready = false;
+			this.active = false // set to false so you don't interrupt the page change with another page change		
 			window.location = this.gallery.next;
 			return true;
 		}
@@ -251,5 +264,3 @@ const eXHentaiCtrl = new (class exHentaiCtrl {
 		window.scrollTo(0, initScroll);
 	};
 })()
-
-window.addEventListener("load", _ => eXHentaiCtrl.initPage());
