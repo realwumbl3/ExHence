37f10e0b576a1b88fe08b1c93895cfc9aaaed059
realwumbl3 <wumbl3@gmail.com>
2024-05-22 21:55:15 -0400

    OwO

--- src/header.js
+++ src/header.js
@@ -9,41 +9,36 @@ import showHelper from './helper.js';
  * @param {HTMLElement} exheader - the vanilla exheader element
  */
 export default function (exheader) {
-  this.verbose && console.log('[ExHentaiCTRL] | Extending vanilla header...');
-  html`
+	this.verbose && console.log('[ExHentaiCTRL] | Extending vanilla header...');
+	html`
 		<div><span this=opts class="nbw custom" zyx-click="${showOptions.bind(this)}">CTRL Options</span></div>
 		<div><span this=help class="nbw custom" zyx-click="${showHelper.bind(this)}">Show Hotkeys</span></div>
 		<div><span this=cleartab class="nbw custom" >Clear History.</span></div>
     `
-    .pass(({ cleartab }) => {
-      cleartab.addEventListener('click', () => {
-        cleartab.textContent = 'CTRL History Cleared!';
-        setTimeout(() => cleartab.textContent = 'Clear History.', 1000);
-        this.clearState();
-      });
-    })
-    .appendTo(exheader)
+		.pass(({ cleartab }) => {
+			cleartab.addEventListener('click', () => {
+				cleartab.textContent = 'CTRL History Cleared!';
+				setTimeout(() => cleartab.textContent = 'Clear History.', 1000);
+				this.clearState();
+			});
+		})
+		.appendTo(exheader)
 
-
-
-
-  html`
+	html`
     <span class="EhLogo" style="background-image: url('https://exhentai.org/favicon.ico');"></span>
-  `.prependTo(exheader);
-
-
-  css`
-    .EhLogo {
-      display: inline-block;
-      width: 2em;
-      aspect-ratio: 1/1;
-      background-size: contain;
-      background-repeat: no-repeat;
-      margin-right: 0.5em;
-    }
-  `
-
-
-
+	`.prependTo(exheader);
 
 }
+
+css`
+
+.EhLogo {
+	display: inline - block;
+	width: 2em;
+	aspect - ratio: 1 / 1;
+	background - size: contain;
+	background - repeat: no - repeat;
+	margin - right: 0.5em;
+}	
+
+`
