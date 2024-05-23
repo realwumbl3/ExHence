811f58edb6fdb1895528e60180f7242dc69dda42
realwumbl3 <wumbl3@gmail.com>
2024-05-22 21:54:14 -0400

    added a custom logo to header uwu

--- src/header.js
+++ src/header.js
@@ -1,4 +1,4 @@
-import zyX, { html } from "./zyX-es6.js";
+import zyX, { html, css } from "./zyX-es6.js";
 
 import ExHentaiCtrl from './exhentai-ctrl.js';
 import showOptions from './options.js';
@@ -9,11 +9,41 @@ import showHelper from './helper.js';
  * @param {HTMLElement} exheader - the vanilla exheader element
  */
 export default function (exheader) {
-    this.verbose && console.log('[ExHentaiCTRL] | Extending vanilla header...');
-    html`
+  this.verbose && console.log('[ExHentaiCTRL] | Extending vanilla header...');
+  html`
 		<div><span this=opts class="nbw custom" zyx-click="${showOptions.bind(this)}">CTRL Options</span></div>
 		<div><span this=help class="nbw custom" zyx-click="${showHelper.bind(this)}">Show Hotkeys</span></div>
-		<div><span this=cleartab class="nbw custom" zyx-click="${this.clearState.bind(this)}">Clear History.</span></div>
+		<div><span this=cleartab class="nbw custom" >Clear History.</span></div>
     `
-        .appendTo(exheader)
+    .pass(({ cleartab }) => {
+      cleartab.addEventListener('click', () => {
+        cleartab.textContent = 'CTRL History Cleared!';
+        setTimeout(() => cleartab.textContent = 'Clear History.', 1000);
+        this.clearState();
+      });
+    })
+    .appendTo(exheader)
+
+
+
+
+  html`
+    <span class="EhLogo" style="background-image: url('https://exhentai.org/favicon.ico');"></span>
+  `.prependTo(exheader);
+
+
+  css`
+    .EhLogo {
+      display: inline-block;
+      width: 2em;
+      aspect-ratio: 1/1;
+      background-size: contain;
+      background-repeat: no-repeat;
+      margin-right: 0.5em;
+    }
+  `
+
+
+
+
 }
