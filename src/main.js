389af2ef552bdd4dcd2c71d8a03ef058b4b76cae
realwumbl3 <wumbl3@gmail.com>
2024-05-24 20:25:15 -0400

    more view header buttons!@!!

--- src/main.js
+++ src/main.js
@@ -25,7 +25,7 @@ import ExView from "./view.js";
 
 export default class Exhence {
 	constructor() {
-		this.verbose = false;
+		this.verbose = true;
 
 		this.options = {
 			autoScrollPadding: 250,
